d5875c86b521adbabd0d5e9f25c60af1d5f72c48
Bogna Knychala <bogna.ka@gmail.com>
2015-06-17 09:56:16 +0200

    Implement auto refresh every hour

--- static/scripts/modules/jenkins.js
+++ static/scripts/modules/jenkins.js
@@ -12,16 +12,17 @@ define('jenkins', ['jquery', 'spinner', 'mustache'], function($, spinner, mustac
 
     function createItems(screenConfig, cb) {
         var $screen = $(document.getElementById(screenConfig['id'])),
-            items = screenConfig['screen_items'];
+            items = screenConfig['screen_items'],
+            updateInterval = screenConfig['update_interval'];
 
         if (items) {
             for (var i=0; i<items.length; i++) {
-                createItem(items[i], $screen, cb);
+                createItem(items[i], $screen, updateInterval, cb);
             }
         }
     }
 
-    function createItem(item, $screen, cb) {
+    function createItem(item, $screen, updateInterval, cb) {
         var request = getRequest(item);
 
         request.done(function(jenkinsBuildData) {
@@ -44,6 +45,31 @@ define('jenkins', ['jquery', 'spinner', 'mustache'], function($, spinner, mustac
             }
             cb();
         });
+
+        setInterval(function() {
+            spinner.showSpinner(item);
+            var request = getRequest(item);
+            request.done(function(jenkinsBuildData) {
+                var rendered = mustache.render(template, jenkinsBuildData),
+                    failedCount = jenkinsBuildData['failed_runs'].length, $dashboardItem, $counter;
+
+                $dashboardItem = $screen.find('#' + jenkinsBuildData['name']);
+                spinner.hideSpinner(jenkinsBuildData['name']);
+                $dashboardItem.html(rendered);
+
+                $counter = $($dashboardItem.find('.results-counter')[0]);
+                if (jenkinsBuildData['child_runs_count'] > 0) {
+                    $counter.text(failedCount + '/' + jenkinsBuildData['child_runs_count']);
+                }
+
+                if (jenkinsBuildData['status'] === 'FAILURE') {
+                    $dashboardItem.find('h2').addClass('failed');
+                } else {
+                    $dashboardItem.find('h2').addClass('success');
+                }
+                cb();
+            });
+        }, updateInterval)
     }
 
     return {
