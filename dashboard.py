f27053fb3353f35f5fe161eb250757e33a9d6242
Bogna Knychala <bogna.ka@gmail.com>
2015-09-16 18:36:01 +0200

    Add support for jenkins api

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,7 @@ import datetime
 from flask import Flask, jsonify
 from flask.templating import render_template
 from jenkinsapi.custom_exceptions import NoBuildData
+import os
 
 from jenkinsapi.jenkins import Jenkins
 from requests.exceptions import ConnectionError
@@ -14,6 +15,8 @@ app = Flask(__name__)
 def get_jenkins():
     return Jenkins(get_config()['sources']['jenkins']['url'])
 
+def get_jenkins_api():
+    return Jenkins(get_config()['sources']['jenkins-api']['url'], username=os.environ['JENKINS_USER'], password=os.environ['JENKINS_PASS'])
 
 def get_config():
     return json.load(open('config.json'))
@@ -63,6 +66,53 @@ def get_build_data(build_name):
         'has_failed_runs': (len(failed_runs) != 0),
         'child_runs_count': child_runs_count,
         'failure_percentage': len(failed_runs) * 100 / child_runs_count
+    }
+
+    try:
+        last_success = get_time_ago(build.get_last_stable_build().get_timestamp()),
+    except NoBuildData:
+        last_success = '???'
+
+    return_val['last_success'] = last_success
+
+    return jsonify(return_val)
+
+@app.route('/jenkins_api_results/<build_name>', methods=['GET'])
+def get_build_data_api(build_name):
+    jenkins_instance = get_jenkins_api()
+    if jenkins_instance is not None:
+        build = jenkins_instance[build_name]
+    else:
+        raise ConnectionError("Connection with Jenkins failed")
+
+    last_build = build.get_last_build()
+    last_build_number = build.get_last_buildnumber()
+    child_runs = last_build.get_matrix_runs()
+    child_runs_count = 0
+    failed_runs = []
+
+    has_next = True
+    while has_next:
+        try:
+            current_build = child_runs.next()
+        except StopIteration:
+            has_next = False
+
+        if has_next:
+            child_runs_count += 1
+            if current_build.get_number() == last_build_number and current_build.get_status() == 'FAILURE':
+                failed_runs.append({
+                    'name': current_build.name.split('\xbb')[1].split(',')[0]
+                })
+
+    return_val = {
+        'name': build_name,
+        'status': last_build.get_status(),
+        'hours_ago': get_time_ago(last_build.get_timestamp()),
+        'failed_runs': failed_runs,
+        'has_failed_runs': (len(failed_runs) != 0),
+        'child_runs_count': child_runs_count,
+        'failure_percentage': len(failed_runs) * 100 / child_runs_count if (len(failed_runs) != 0) else 0
     }
 
     try:
--- static/scripts/modules/screen.js
+++ static/scripts/modules/screen.js
@@ -1,10 +1,13 @@
-define('screen', ['jquery', 'jenkins', 'spinner', 'mustache', 'pie-chart'], function($, jenkins, spinner, mustache, pieChart) {
+define('screen', ['jquery', 'jenkins', 'jenkins-api', 'spinner', 'mustache', 'pie-chart'], function($, jenkins, jenkinsApi, spinner, mustache, pieChart) {
     'use strict';
 
     function createScreen(screenConfig) {
         switch (screenConfig['data_source']) {
             case 'jenkins':
                 jenkins.createItems(screenConfig, pieChart.drawPies);
+                break;
+            case 'jenkins-api':
+                jenkinsApi.createItems(screenConfig, pieChart.drawPies);
                 break;
             default:
                 throw new Error('Wrong data source provided');
