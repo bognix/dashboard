60773c7ec4ab1db9126676bac0120414c195a041
Bogna Knychala <bogna.ka@gmail.com>
2015-10-06 18:18:23 +0200

    Add phantomas artifact to dashboard

--- dashboard.py
+++ dashboard.py
@@ -6,6 +6,7 @@ from jenkinsapi.custom_exceptions import NoBuildData
 import os
 
 from jenkinsapi.jenkins import Jenkins
+from jenkinsapi.artifact import Artifact
 from requests.exceptions import ConnectionError
 
 
@@ -23,6 +24,8 @@ def get_jenkins(jenkins_name):
 def get_config():
     return json.load(open('config.json'))
 
+def get_item_config(build_name):
+    return get_config()['items'][build_name]
 
 @app.route('/')
 def index():
@@ -35,6 +38,7 @@ def index():
 @app.route('/jenkins_results/<jenkins_name>/<build_name>', methods=['GET'])
 def get_build_data(jenkins_name, build_name):
     jenkins_instance = get_jenkins(jenkins_name)
+    item_config = get_item_config(build_name)
     if jenkins_instance is not None:
         build = jenkins_instance[build_name]
     else:
@@ -45,30 +49,34 @@ def get_build_data(jenkins_name, build_name):
     child_runs = last_build.get_matrix_runs()
     child_runs_count = 0
     failed_runs = []
-
-    has_next = True
-    while has_next:
-        try:
-            current_build = child_runs.next()
-        except StopIteration:
-            has_next = False
-
-        if has_next:
-            child_runs_count += 1
-            if current_build.get_number() == last_build_number and current_build.get_status() == 'FAILURE':
-                failed_runs.append({
-                    'name': current_build.name.split('\xbb')[1].split(',')[0]
-                })
-
     return_val = {
         'name': build_name,
-        'status': last_build.get_status(),
+        'status': last_build.get_status(),    
         'hours_ago': get_time_ago(last_build.get_timestamp()),
-        'failed_runs': failed_runs,
-        'has_failed_runs': (len(failed_runs) != 0),
-        'child_runs_count': child_runs_count,
-        'failure_percentage': len(failed_runs) * 100 / child_runs_count if (child_runs_count != 0) else 100
     }
+
+    if item_config.has_key('artifact'):
+        output = Artifact('output', item_config['artifact'], last_build).get_data()
+        return_val['artifact_output'] = output
+    else:
+        has_next = True
+        while has_next:
+            try:
+                current_build = child_runs.next()
+            except StopIteration:
+                has_next = False
+
+            if has_next:
+                child_runs_count += 1
+                if current_build.get_number() == last_build_number and current_build.get_status() == 'FAILURE':
+                    failed_runs.append({
+                        'name': current_build.name.split('\xbb')[1].split(',')[0]
+                    })
+
+        return_val['failed_runs'] = failed_runs
+        return_val['has_failed_runs'] = (len(failed_runs) != 0)
+        return_val['child_runs_count'] = child_runs_count
+        return_val['failure_percentage'] = len(failed_runs) * 100 / child_runs_count if (child_runs_count != 0) else 100
 
     try:
         last_success = get_time_ago(build.get_last_stable_build().get_timestamp()),
--- static/scripts/modules/jenkins.js
+++ static/scripts/modules/jenkins.js
@@ -30,8 +30,9 @@ define('jenkins', ['jquery', 'spinner', 'mustache'], function($, spinner, mustac
 
         request.done(function(jenkinsBuildData) {
             var rendered = mustache.render(template, jenkinsBuildData),
-                failedCount = jenkinsBuildData['failed_runs'].length, $dashboardItem, $counter;
+                $dashboardItem, $counter, failedCount;
 
+            failedCount = Array.isArray(jenkinsBuildData['failed_runs']) ? jenkinsBuildData['failed_runs'].length : 0
             $dashboardItem = $screen.find('#' + jenkinsBuildData['name']);
             spinner.hideSpinner(jenkinsBuildData['name']);
             $dashboardItem.html(rendered);
@@ -39,6 +40,8 @@ define('jenkins', ['jquery', 'spinner', 'mustache'], function($, spinner, mustac
             $counter = $($dashboardItem.find('.results-counter')[0]);
             if (jenkinsBuildData['child_runs_count'] > 0) {
                 $counter.text(failedCount + '/' + jenkinsBuildData['child_runs_count']);
+            } else {
+                $($dashboardItem.find('.pie')[0]).hide();
             }
 
             if (jenkinsBuildData['status'] === 'FAILURE') {
