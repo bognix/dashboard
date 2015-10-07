e197f52d48db2e297710cf665513fc81e66ef9b0
Bogna Knychala <bogna.ka@gmail.com>
2015-10-07 13:07:48 +0200

    Fix type error

--- static/scripts/modules/jenkins.js
+++ static/scripts/modules/jenkins.js
@@ -57,8 +57,9 @@ define('jenkins', ['jquery', 'spinner', 'mustache'], function($, spinner, mustac
             var request = getRequest(item);
             request.done(function(jenkinsBuildData) {
                 var rendered = mustache.render(template, jenkinsBuildData),
-                    failedCount = jenkinsBuildData['failed_runs'].length, $dashboardItem, $counter;
+                $dashboardItem, $counter, failedCount;
 
+                failedCount = Array.isArray(jenkinsBuildData['failed_runs']) ? jenkinsBuildData['failed_runs'].length : 0
                 $dashboardItem = $screen.find('#' + jenkinsBuildData['name']);
                 spinner.hideSpinner(jenkinsBuildData['name']);
                 $dashboardItem.html(rendered);
@@ -66,6 +67,8 @@ define('jenkins', ['jquery', 'spinner', 'mustache'], function($, spinner, mustac
                 $counter = $($dashboardItem.find('.results-counter')[0]);
                 if (jenkinsBuildData['child_runs_count'] > 0) {
                     $counter.text(failedCount + '/' + jenkinsBuildData['child_runs_count']);
+                } else {
+                    $($dashboardItem.find('.pie')[0]).hide();
                 }
 
                 if (jenkinsBuildData['status'] === 'FAILURE') {
